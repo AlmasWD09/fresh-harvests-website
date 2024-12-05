04e4a5c2997f685ee39fc249f2e7ec6f6c199b93
Almas Hossain <almashossain7384@gmail.com>
2024-12-05 22:58:58 +0600

    banner section design ok

--- src/pages/home/about/About.jsx
+++ src/pages/home/about/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <>
       <section className="container mx-auto px-6 ">
-        <div className="flex justify-center items-center gap-6">
+        <div className="flex flex-col lg:flex-row justify-center items-center gap-6">
           <div className="w-full lg:w-[50%] flex justify-center ">
             <img
               className="w-full object-cover"
@@ -14,14 +14,12 @@ const About = () => {
           </div>
           <div className="w-full lg:w-[50%] space-y-4">
           <button className="text-primary bg-primary/10 font-medium py-1 px-2 rounded">About Us</button>
-            <h3 className="text-5xl font-Rubik font-medium">About Fresh Harvest</h3>
+            <h3 className="font-Rubik font-medium text-3xl md:text-4xl lg:text-5xl">About Fresh Harvest</h3>
             <p className="max-w-lg text-base font-Questrial text-[#4A4A52]">Welcome to Fresh Harvest, your premier destination for high-quality and fresh produce. We are passionate about providing you with the finest fruits, vegetables, and salad ingredients to nourish your body and delight your taste buds. With a commitment to excellence, sustainability, and customer satisfaction, Fresh Harvest is here to revolutionize your grocery shopping experience.</p>
             <button className="text-[#FF6A1A] font-semibold border border-[#FF6A1A] py-1 px-3 rounded">Read More</button>
           </div>
-
         </div>
       </section>
-
     </>
   )
 }
--- src/pages/home/freshProducts/FreshProducts.jsx
+++ src/pages/home/freshProducts/FreshProducts.jsx
@@ -1,8 +1,86 @@
+import { useEffect, useState } from "react";
 
 
 const FreshProducts = () => {
+  const [products, setProducts] = useState([]);
+
+
+  // Data load
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://api-fresh-harvest.code-commando.com/api/v1/products");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error loading data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+console.log(products)
   return (
-    <div>FreshProducts...</div>
+ <>
+ <h1>product page</h1>
+{/* 
+ <section className="container mx-auto px-4 py-8">
+
+<div className="flex justify-between mb-2">
+  <div>
+    <h2 className="uppercase text-primary ">Summer</h2>
+    <p className="font-semibold">Big Deal</p>
+  </div>
+  <div className="flex gap-2">
+    <span className="border border-primary rounded-full flex justify-center items-center h-8 w-8"><BsArrowLeft /></span>
+    <span className="border border-primary rounded-full flex justify-center items-center h-8 w-8"><BsArrowRight /></span>
+  </div>
+</div>
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+  {products.map((product, idx) => (
+    <div
+      key={idx}
+      className="bg-white rounded-lg shadow-md p-2 hover:shadow-2xl transition duration-300"
+    >
+      <div className="relative h-[220px] text-center">
+        <Image
+        onClick={() => handleAddToCart(product.id)} 
+          src={product.image}
+          alt={product.name}
+          width={300}
+          height={300}
+          className="max-h-[200px] w-full bg-primaryGray/50 object-contain bg-center rounded-md "
+        />
+        <span className="absolute h-[66px] w-12 top-0 right-2 px-3 flex justify-center items-center bg-primary text-white text-sm rounded-b-full">
+          Up to 40%
+        </span>
+      </div>
+      <div className="mt-4">
+        <div className="flex items-center space-x-1 text-yellow-500">
+          {[...Array(Math.round(product.rating))].map((_, i) => (
+            <span key={i}>&#9733;</span>
+          ))}
+          <span className="text-black">({product.rating})</span>
+        </div>
+        <div className="flex justify-between">
+          <h5 className="font-semibold ">{product.name}</h5>
+          <h4 className="text-lg font-semibold">BDT {product.price}</h4>
+        </div>
+      </div>
+      <div className="mt-4">
+        <button className="w-full py-2 border border-primary hover:text-white font-semibold rounded hover:bg-primary transition duration-300">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  ))}
+</div>
+<div className="text-center mt-8">
+  <button className="px-6 py-2 bg-primary/90 text-white rounded hover:bg-primary transition duration-300">
+    See More
+  </button>
+</div>
+</section> */}
+ </>
   )
 }
 
